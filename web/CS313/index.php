f30bd640b66ca8742279062ec7715f4543b7608f
lehi-logan-lee <lihai0119@gmail.com>
2020-05-25 05:49:07 -0600

    Changed the file extentions

--- web/CS313/index.php
+++ web/CS313/index.php
@@ -12,15 +12,15 @@
   <li><a href="prove02.php">prove02</a></li>
   <li><a href="prove03/index.php">prove03</a></li>
   <li><a href="prove04.html">prove04</a></li>
-  <li><a href="prove05/index.html">prove05</a></li>
-  <li><a href="prove06.html">prove06</a></li>
-  <li><a href="prove07.html">prove07</a></li>
-  <li><a href="prove08.html">prove08</a></li>
-  <li><a href="prove09.html">prove09</a></li>
-  <li><a href="prove10.html">prove10</a></li>
-  <li><a href="prove11.html">prove11</a></li>
-  <li><a href="prove12.html">prove12</a></li>
-  <li><a href="prove13.html">prove13</a></li>
+  <li><a href="prove05/index.php">prove05</a></li>
+  <li><a href="prove06.php">prove06</a></li>
+  <li><a href="prove07.php">prove07</a></li>
+  <li><a href="prove08.php">prove08</a></li>
+  <li><a href="prove09.php">prove09</a></li>
+  <li><a href="prove10.php">prove10</a></li>
+  <li><a href="prove11.php">prove11</a></li>
+  <li><a href="prove12.php">prove12</a></li>
+  <li><a href="prove13.php">prove13</a></li>
   </ul>
 
 </body>
