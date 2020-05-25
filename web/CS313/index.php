df0821f295bc30087270b6363f3cd2a7bab2b9e0
lehi-logan-lee <lihai0119@gmail.com>
2020-05-25 05:42:09 -0600

    changed index URL

--- web/CS313/index.php
+++ web/CS313/index.php
@@ -12,7 +12,7 @@
   <li><a href="prove02.php">prove02</a></li>
   <li><a href="prove03/index.php">prove03</a></li>
   <li><a href="prove04.html">prove04</a></li>
-  <li><a href="prove05.html">prove05</a></li>
+  <li><a href="prove05/index.html">prove05</a></li>
   <li><a href="prove06.html">prove06</a></li>
   <li><a href="prove07.html">prove07</a></li>
   <li><a href="prove08.html">prove08</a></li>
