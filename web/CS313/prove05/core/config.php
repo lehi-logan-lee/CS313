75deb9283250426318a3c22f748dacf9ca6a74ee
lehi-logan-lee <lihai0119@gmail.com>
2020-05-25 22:23:05 -0600

    Test

--- web/CS313/prove05/core/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-$host = "localhost";
-$username = "root";
-$password = "";
-$dbname = "register_func";
-$dbPort = '5432';
-?>
--- web/CS313/prove05/index.php
+++ web/CS313/prove05/index.php
@@ -1,5 +1,5 @@
 <?php
-require 'dbConnect.php';
+require 'dbconnect.php';
 $db = get_db();
 
 
