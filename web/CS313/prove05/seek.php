4f258450db6ac26643c0f3d76104782dae5c202d
lehi-logan-lee <lihai0119@gmail.com>
2020-05-26 04:18:48 -0600

    Changed search result

--- web/CS313/prove05/seek.php
+++ web/CS313/prove05/seek.php
@@ -12,5 +12,5 @@ $stmt = $db->query($sql);
 <!-- 検索キーワードと検索結果件数を<h1>で括って表示する
  $total_results = $pdo->query("SELECT COUNT(code)FROM goods WHERE name='$s'");
  $total_results = $pdo->query("SELECT COUNT(code) FROM(SELECT * FROM goods WHERE name='$s')");-->
-<h3><?php echo $s; ?>search result<span>（ <?php echo $stmt->fetchColumn(); ?> found）</span></h3>
+<h3><?php echo $s; ?> search result<span>（ <?php echo $stmt->fetchColumn(); ?> found）</span></h3>
 
