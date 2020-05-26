b8c015218939dcad8139640d21f41cefc4d81b88
lehi-logan-lee <lihai0119@gmail.com>
2020-05-26 04:15:12 -0600

    deleted unneeded file

--- web/CS313/prove05/seek.php
+++ web/CS313/prove05/seek.php
@@ -5,12 +5,12 @@ $s = $_GET["s"];
 $goods = $db->query("SELECT * FROM goods WHERE name LIKE '%$s%'");
 require 't_index.php';
 
-$sql = "SELECT COUNT(code)FROM goods WHERE name='$s'";
+$sql = "SELECT COUNT(code)FROM goods WHERE name LIKE '%$s%'";
 $stmt = $db->query($sql);
 ?>
 
 <!-- 検索キーワードと検索結果件数を<h1>で括って表示する
  $total_results = $pdo->query("SELECT COUNT(code)FROM goods WHERE name='$s'");
  $total_results = $pdo->query("SELECT COUNT(code) FROM(SELECT * FROM goods WHERE name='$s')");-->
-<h3><?php echo $s; ?>の検索結果<span>（ <?php echo $stmt->fetchColumn(); ?> 件）</span></h3>
+<h3><?php echo $s; ?>search result<span>（ <?php echo $stmt->fetchColumn(); ?> found）</span></h3>
 
