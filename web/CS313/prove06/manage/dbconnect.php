8eef797f3135bc262349b9a3611c86dacb1f39ad
lehi-logan-lee <lihai0119@gmail.com>
2020-05-29 04:45:35 -0600

    PHP Data Modification

---
+++ web/CS313/prove06/manage/dbconnect.php
@@ -0,0 +1,39 @@
+<?php
+function get_db() {
+	$db = NULL;
+
+	try {
+		// default Heroku Postgres configuration URL
+		$dbUrl = getenv('DATABASE_URL');
+
+    // Get the various parts of the DB Connection from the URL
+		$dbopts = parse_url($dbUrl);
+
+		$dbHost = $dbopts["host"];
+		$dbPort = $dbopts["port"];
+		$dbUser = $dbopts["user"];
+		$dbPassword = $dbopts["pass"];
+		$dbName = ltrim($dbopts["path"],'/');
+
+		// Create the PDO connection
+		$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+		// this line makes PDO give us an exception when there are problems, and can be very helpful in debugging!
+		$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+	}
+	catch (PDOException $ex) {
+		// If this were in production, you would not want to echo
+		// the details of the exception.
+		echo "Error connecting to DB. Details: $ex";
+		die();
+	}
+
+	return $db;
+}
+
+function img_tag($code) {
+    if (file_exists("../prove05/images/$code.jpg")) $name = $code;
+    else $name = 'noimage';
+    return '<img src="../prove05/images/' . $name . '.jpg" alt="">';
+}
+?>
---
+++ web/CS313/prove06/manage/upload.php
@@ -0,0 +1,17 @@
+<?php
+  require 'dbconnect.php';
+  
+  $error = '';
+  if (@$_POST['submit']) {
+    $code = $_POST['code'];
+    if (move_uploaded_file($_FILES['pic']['tmp_name'], "../prove05/images/$code.jpg")) {
+      header('Location: index.php');
+      exit();
+    } else {
+      $error .= 'ファイルを選択してください。<br>';
+    }
+  } else {
+    $code = $_GET['code'];
+  }
+  require 't_upload.php';
+?>
