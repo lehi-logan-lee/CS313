ccf814c2ae6417221def55343d0604345f53e8b3
lehi-logan-lee <lihai0119@gmail.com>
2020-06-07 08:29:36 -0600

    test

--- web/CS313/prove07/confirm.php
+++ web/CS313/prove07/confirm.php
@@ -1,3 +1,23 @@
+<?php
+  require 'dbconnect.php';
+  $rows = array();
+  $sum = 0;
+  $db = get_db();
+  if (!isset($_SESSION['cart'])) $_SESSION['cart'] = array();
+  if (@$_POST['submit']) {
+    @$_SESSION['cart'][$_POST['code']] = $_POST['num'];
+  }
+  foreach($_SESSION['cart'] as $code => $num) {
+    $st = $db->prepare("SELECT * FROM goods WHERE code=?");
+    $st->execute(array($code));
+    $row = $st->fetch();
+    $st->closeCursor();
+    $row['num'] = strip_tags($num);
+    $sum += $num * $row['price'];
+    $rows[] = $row;
+  }
+  require 't_cart.php';
+?>
 
 <!DOCTYPE html>
 <html lang="en"><head>
@@ -5,6 +25,7 @@
 <link href="shop.css" type="text/css" rel="stylesheet" />
 </head>
 <body>
+<a href="index.php">Browse</a>
 <h1>Purchase Confirmation</h1>
 	
 		
