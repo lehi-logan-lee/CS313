10fcce296e3fb16bff076e6d05b870376534a21f
lehi-logan-lee <lihai0119@gmail.com>
2020-06-07 08:12:29 -0600

    test again

--- web/CS313/prove07/confirm.php
+++ web/CS313/prove07/confirm.php
@@ -2,26 +2,3 @@
 //session_start();
 require 'cart.php';
 ?>
-<!DOCTYPE html>
-<html lang="en"><head>
-<TITLE>Confirmation</TITLE>
-<link href="shop.css" type="text/css" rel="stylesheet" />
-</head>
-<body>
-<h1>Purchase Confirmation</h1>
-	
-		
-
-
-<?php
-  if (@$_POST['submit']) {
-    $address = htmlspecialchars($_POST['address']);
-    $city = htmlspecialchars($_POST['city']);
-    $state = htmlspecialchars($_POST['state']);
-    echo "<b>Shipping Address: </b>" .$address .", " .$city .", " .$state ."<br>";
-    $_SESSION['cart'] = null;
-    exit();
-  }
-?>
-</body>
-</html>
