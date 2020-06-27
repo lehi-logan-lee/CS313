95a648b405ea87e82787f72a1906ad8d95f12ab4
lehi-logan-lee <lihai0119@gmail.com>
2020-06-26 22:43:49 -0600

    added links for week4 and 9

--- web/CS313/index.php
+++ web/CS313/index.php
@@ -11,12 +11,12 @@
   <ul>
   <li><a href="prove02.php">prove02</a></li>
   <li><a href="prove03/index.php">prove03</a></li>
-  <li><a href="prove04.html">prove04</a></li>
+  <li><a href="https://github.com/lehi-logan-lee/CS313/blob/master/db/prove04Db.sql">prove04</a></li>
   <li><a href="prove05/index.php">prove05</a></li>
   <li><a href="prove06/index.php">prove06</a></li>
   <li><a href="prove07/index.php">prove07</a></li>
   <li><a href="prove08.php">prove08</a></li>
-  <li><a href="prove09.php">prove09</a></li>
+  <li><a href="../../node-js-getting-started/public/index.html">prove09</a></li>
   <li><a href="prove10.php">prove10</a></li>
   <li><a href="prove11.php">prove11</a></li>
   <li><a href="prove12.php">prove12</a></li>
